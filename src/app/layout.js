7fa726400a18acaa354b00616752809f4da4e8fb
Karema <karemahamdy51@gmail.com>
2024-11-25 03:02:08 +0200

    feat: add favicon and fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Josefin_Sans } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefin.variable} ${josefin.variable} antialiased`}
       >
         {children}
       </body>
