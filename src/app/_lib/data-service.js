348a2252cc8164ae737b329407fac4413b9ff112
Karema <karemahamdy51@gmail.com>
2024-11-26 01:57:01 +0200

    fix: err of pricing

--- src/app/_lib/data-service.js
+++ src/app/_lib/data-service.js
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { eachDayOfInterval } from "date-fns";
+// import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
 
 /////////////
--- src/app/_lib/supabase.js
+++ src/app/_lib/supabase.js
@@ -1,8 +1,4 @@
 
-import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = 'https://tlkyzpirifkgkmylfzwd.supabase.co'
-// const supabaseKey = process.env.SUPABASE_KEY
-// const supabase = createClient(supabaseUrl, supabaseKey)
 
 import { createClient } from "@supabase/supabase-js";
 
