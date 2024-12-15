7b9d0ddfa124cdec6c1daf53b2d552f6fdac2438
Karema <karemahamdy51@gmail.com>
2024-12-15 03:10:51 +0200

    delete reservation

--- src/app/_lib/actions.js
+++ src/app/_lib/actions.js
@@ -1,13 +1,34 @@
 "use server";
+
 import { getBookings } from "./data-service";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+export async function updateGuest(formData) {
 
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
+}
 
 export async function createBooking(bookingData, formData) {
   const session = await auth();
+  if (!session) throw new Error("You must be logged in");
 
   const newBooking = {
     ...bookingData,
@@ -21,7 +42,7 @@ export async function createBooking(bookingData, formData) {
     status: "unconfirmed",
   };
 
-  const { error } = await supabase.from("Bookings").insert([newBooking]);
+  const { error } = await supabase.from("bookings").insert([newBooking]);
 
   if (error) throw new Error("Booking could not be created");
 
@@ -31,13 +52,8 @@ export async function createBooking(bookingData, formData) {
 }
 
 export async function deleteBooking(bookingId) {
-
-  const guestBookings = await getBookings(session.user.guestId);
-  const guestBookingIds = guestBookings.map((booking) => booking.id);
-
-  if (!guestBookingIds.includes(bookingId))
-    throw new Error("You are not allowed to delete this booking");
-
+  
+   
   const { error } = await supabase
     .from("Bookings")
     .delete()
